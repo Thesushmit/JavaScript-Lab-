136ca3611b8444755773d3f87c15b022db2d8c64
Thesushmit <sushmitp1@gmail.com>
2025-07-11 21:49:56 +0530

    final prac uploaded

---
+++ Practical 1/Student.js
@@ -0,0 +1,5 @@
+function Inputname() {
+    var name = prompt("HEY WELCOME TO SIT NAGPUR , Please enter your name!")
+    document.getElementById('change').innerText = "Hello " + name;
+    console.log("hello" + name)
+}
---
+++ Practical 1/printname.js
@@ -0,0 +1,10 @@
+function printname(){
+	a = prompt("Enter your name");
+	alert("your name is "+a);
+	document.write("your name is "+a);
+	
+}
+function useconsol(){
+	var a = prompt("Enter name");
+	console.log(a);
+}
